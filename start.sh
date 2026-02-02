d78a0ca67879b01d06a8eb25632b14e376000220
Gradi-Makanga <gradimakanga86@gmail.com>
2026-02-02 14:21:13 +0100

    new start

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "Starting Shiny..."
+echo "Working dir:"
+pwd
+echo "Listing root:"
+ls -la
+
 cd ui
+echo "Now in:"
+pwd
+echo "Listing ui:"
+ls -la
+
 R -e "shiny::runApp('.', host='0.0.0.0', port=as.integer(Sys.getenv('PORT', 3838)))"
